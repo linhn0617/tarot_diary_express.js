b7be077505bc8021c6e5778cba43e0a82fc18b9b
linhn0617 <yyo833782@gmail.com>
2025-06-09 00:10:33 +0800

    feat: refactor app.ts and implement modular route structure

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
+import express from 'express';
 import { Request, Response } from 'express';
-const express = require('express')
-const app = express()
-const port = 3000
+import routes from './routes';;
+const app = express();
+const port = 3000;
 
+app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
-})
+app.use('/', routes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
